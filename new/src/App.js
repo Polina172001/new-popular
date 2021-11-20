6e5b0605f9602ee54699fc2acb48f217bc4542c6
Polina <polina17012001@mail.ru>
2021-11-20 18:52:50 +0300

    version1

---
+++ new/src/App.js
@@ -0,0 +1,116 @@
+import './App.css';
+
+import { useState } from 'react';
+
+function New(props) {
+  return (
+    <div className="wrap-item wrap-item-new">
+      <span className="label">New!</span>
+      {props.children}
+    </div>
+  )
+};
+
+function Popular(props) {
+  return (
+    <div className="wrap-item wrap-item-popular">
+      <span className="label">Popular!</span>
+      {props.children}
+    </div>
+  )
+};
+
+function Article(props) {
+  return (
+    <div className="item item-article">
+      <h3><a href="#">{props.title}</a></h3>
+      <p className="views">Прочтений: {props.views}</p>
+    </div>
+  )
+};
+
+function Video(props) {
+  return (
+    <div className="item item-video">
+      <iframe src={props.url} frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+      <p className="views">Просмотров: {props.views}</p>
+    </div>
+  )
+};
+
+function HighLight(Component, wrapperSelect) {
+  return (props) => {
+    const Wrapper = wrapperSelect(props.views)
+    const HighLightComponent = Wrapper 
+    ? (<Wrapper><Component {...props} /></Wrapper>)
+    : (<Component {...props} />)
+
+    return HighLightComponent
+  }
+}
+
+const wrapperSelect = (views) => {
+  if (views > 1000) return Popular
+  if (views < 100) return New
+  return null
+}
+
+function List(props) {
+  return props.list.map(item => {
+    let Component = null
+    switch (item.type) {
+      case 'video':
+        Component = HighLight(Video, wrapperSelect)
+        return (
+          <Component {...item} key={item.url}/>
+        );
+
+      case 'article':
+        Component = HighLight(Article, wrapperSelect)
+        return (
+          <Component {...item} key={item.url}/>
+        );
+      default:
+        return null
+    }
+  });
+};
+
+export default function App() {
+  const [list, _setList] = useState([
+    {
+      type: 'video',
+      url: 'https://www.youtube.com/embed/rN6nlNC9WQA?rel=0&amp;controls=0&amp;showinfo=0',
+      views: 50
+    },
+    {
+      type: 'video',
+      url: 'https://www.youtube.com/embed/dVkK36KOcqs?rel=0&amp;controls=0&amp;showinfo=0',
+      views: 12
+    },
+    {
+      type: 'article',
+      title: 'Невероятные события в неизвестном поселке...',
+      views: 175
+    },
+    {
+      type: 'article',
+      title: 'Секретные данные были раскрыты!',
+      views: 1532
+    },
+    {
+      type: 'video',
+      url: 'https://www.youtube.com/embed/TKmGU77INaM?rel=0&amp;controls=0&amp;showinfo=0',
+      views: 4253
+    },
+    {
+      type: 'article',
+      title: 'Кот Бегемот обладает невероятной...',
+      views: 12,
+    },
+  ]);
+
+  return (
+    <List list={list} />
+  );
+}
